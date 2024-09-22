6b8145430f08ca763c747fcae7084f6fe9d8529d
Oleksandra Podhurska <oleksandra@idunrealestate.com>
2024-09-22 15:12:19 +0300

    Changed plugins

--- src/main/java/ua/edu/ucu/apps/Main.java
+++ src/main/java/ua/edu/ucu/apps/Main.java
@@ -1,8 +1,23 @@
 package ua.edu.ucu.apps;
 
+import ua.edu.ucu.apps.tempseries.TemperatureSeriesAnalysis;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        TemperatureSeriesAnalysis temperatureSeriesAnalysis = new TemperatureSeriesAnalysis();
+        System.out.println(temperatureSeriesAnalysis.summaryStatistics().toString());
+
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String concat = "NAME".concat(input);
+
+
+        String password = "12345";
     }
 }
